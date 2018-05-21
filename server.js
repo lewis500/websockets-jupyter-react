a9d6b6053ffa3380bdf712cd4ad8cd223b1a3b32
lewis500 <lewis500@gmail.com>
2018-05-21 13:53:00 -0700

    added axis component

--- server.js
+++ server.js
@@ -3,43 +3,28 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const webpack = require("webpack");
 const axios = require("axios");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 app.use(require("cors")());
 app.use(bodyParser.json());
-
 
 const config = require("./webpack.config.js");
 const middleware = require("webpack-dev-middleware")(webpack(config));
 
 app.use(middleware);
 
-let mySocket;
-
 io.on("connection", socket => {
   console.log("a user connected");
-  mySocket = socket;
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
 
-// app.get("/laser", (req, res) => {
-//   if (mySocket)
-//     mySocket.emit("laser", null, data => {
-//       res.send({value:data})
-//     });
-// });
 
-app.post('/data',(req,res)=>{
-  // console.log(req.body);
-  if(mySocket){
-    console.log('sending')
-    mySocket.emit("laser",req.body);
-  }
-  res.status(200).send('hello')
-})
-
+app.post("/data", (req, res) => {
+  io.emit("data", req.body);
+  res.status(200).send("hello");
+});
 
 http.listen(3000, () => {
   console.log("listening on *:3000");
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,52 +2,73 @@
 import React, { Component } from "react";
 import socket, { subscribe } from "src/socket";
 import style from "./styleApp.css";
+import Axis from "src/components/Axis";
+import { format } from "d3-format";
+import { scaleLinear } from "d3-scale";
+const axisFormat = format(".2");
 
 const height = 300,
   width = 400,
   mar = { t: 20, l: 40, b: 30, r: 15 };
-const yScale = x => height * (1 - x);
-const xScale = x => width * (x-5)/20;
 
-export default class App extends Component<
-  null,
-  { incoming: number, data: number[][] }
-> {
+export default class App extends Component<null, { data: number[][] }> {
   constructor() {
     super();
     this.state = {
-      incoming: 5,
       data: []
     };
-    subscribe((err, { incoming }: { incoming: number }) => {
-      this.setState({
-        incoming
-      });
-    });
-    socket.on("laser", data => {
-      console.log(data)
+    socket.on("data", data => {
       this.setState({
         data: data
       });
-      // fn(this.state.outgoing);
     });
   }
+
   render() {
+    const yScale = scaleLinear()
+      .range([height, 0])
+      .domain([0,.5])
+      // .domain([0, Math.max(...this.state.data.map(d => d[0]))]);
+
+    const xScale = scaleLinear()
+      .range([0, width])
+      .domain([0,25])
+      // .domain([0, Math.max(...this.state.data.map(d => d[2]))]);
     return (
       <div className={style.main}>
-        <svg width={width} height={height}>
-          {this.state.data.map((d, i) => {
-            return (
-              <rect
-                key={i}
-                className={style.bar}
-                width={xScale(d[2]) - xScale(d[1])}
-                x={xScale(d[1])}
-                height={height - yScale(d[0])}
-                y={yScale(d[0])}
-              />
-            );
-          })}
+        <svg width={width + mar.l + mar.r} height={height + mar.t + mar.b}>
+          <g transform={`translate(${mar.l},${mar.t})`}>
+            <Axis
+              transform={`translate(${0},${height})`}
+              scale={xScale}
+              orient="BOTTOM"
+              format={axisFormat}
+              ticks={5}
+              tickPadding={9}
+            />
+            <Axis
+              ticks={4}
+              tickPadding={9}
+              scale={yScale}
+              orient="LEFT"
+              className={"yAxis"}
+              format={axisFormat}
+            />
+            <g>
+              {this.state.data.map((d, i) => {
+                return (
+                  <rect
+                    key={i}
+                    className={style.bar}
+                    width={xScale(d[2]) - xScale(d[1])}
+                    x={xScale(d[1])}
+                    height={height - yScale(d[0])}
+                    y={yScale(d[0])}
+                  />
+                );
+              })}
+            </g>
+          </g>
         </svg>
       </div>
     );
